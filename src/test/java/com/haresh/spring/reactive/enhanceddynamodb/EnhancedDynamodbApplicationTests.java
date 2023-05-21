83bddee10c767177f9247eebca2970f101e8b274
Haresh Shaha <haresh.shaha@autodesk.com>
2023-05-21 22:58:24 +0530

    Added base spring webflux project.

---
+++ src/test/java/com/haresh/spring/reactive/enhanceddynamodb/EnhancedDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haresh.spring.reactive.enhanceddynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancedDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
